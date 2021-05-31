c21ed56c34606621d556720ae9a4b97882b05692
Ali Abou-bakr <devabakr@gmail.com>
2021-05-31 17:51:07 +0200

    28 may Ali backup

---
+++ app/src/main/java/com/gama/task/ui/fragments/payment/FragmentWayCheckoutViewModel.kt
@@ -0,0 +1,49 @@
+package com.gama.task.ui.fragments.payment
+
+import android.util.Log
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.switchMap
+import com.gama.task.data.repository.GeneralListsRepository
+
+
+import com.gama.task.util.Event
+
+/**
+ * perform business logic and store ui states for [AccountListFragment].
+ */
+class FragmentWayCheckoutViewModel @ViewModelInject constructor(
+    private val generalListsRepository: GeneralListsRepository
+) : ViewModel() {
+
+
+
+
+
+
+
+
+    //result of total of list
+    private val _navigatewithresult = MutableLiveData<String>()
+
+    /**
+     * Immutable version of [_navigateToMainActivity]
+     */
+    val navigatewithresult = _navigatewithresult as LiveData<String>
+
+    //return to page
+    private val _returnwithsuccess = MutableLiveData<Event<Unit>>()
+
+    /**
+     * Immutable version of [_navigateToMainActivity]
+     */
+    val returnwithsuccess = _returnwithsuccess as LiveData<Event<Unit>>
+
+
+
+
+
+
+}
--- app/src/main/java/com/gama/task/ui/main/MainActivity.kt
+++ app/src/main/java/com/gama/task/ui/main/MainActivity.kt
@@ -9,7 +9,10 @@ import android.view.Menu
 import android.view.MotionEvent
 import androidx.activity.viewModels
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
+import androidx.databinding.DataBindingUtil.setContentView
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
@@ -23,23 +26,31 @@ import com.gama.task.databinding.ActivityMainBinding
 import com.gama.task.ui.base.BaseActivity
 import com.gama.task.ui.fragments.cart.GlobalClass
 import com.google.android.material.navigation.NavigationView
+import com.zeugmasolutions.localehelper.LocaleAwareCompatActivity
+import com.zeugmasolutions.localehelper.Locales
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 @AndroidEntryPoint
-class MainActivity : BaseActivity<ActivityMainBinding>() {
+class MainActivity : LocaleAwareCompatActivity() {
+    private val viewModel by viewModels<MainViewModel>()
+    lateinit var LocaleAwareCompatActivity: LocaleAwareCompatActivity
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var navController: NavController
     private lateinit var drawer: DrawerLayout
-    private val viewModel by viewModels<MainViewModel>()
-    override fun getLayoutRes() = R.layout.activity_main
-    override fun init() {
-        val toolbar = binding.toolbar
-        setSupportActionBar(toolbar)
-        drawer = binding.drawerLayout
-        //  drawer=bmainNavigationView
-        //  NavigationUI.setupWithNavController(drawer,navController)
 
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        drawer = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.main_navigation_view)
+        navController = findNavController(R.id.nav_host_fragment)
+        navView.setupWithNavController(navController!!) //the second most important part
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
         val toggle: ActionBarDrawerToggle = ActionBarDrawerToggle(
             this, drawer, toolbar,
             R.string.navigation_drawer_open, R.string.navigation_drawer_close
@@ -47,21 +58,38 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
         drawer.addDrawerListener(toggle)
 
         toggle.syncState()
+
+
+
     }
+
+
+
+
 //val opencart=binding.openCartBtn.setOnClickListener {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        drawer = findViewById(R.id.drawer_layout)
-        val navView: NavigationView = findViewById(R.id.main_navigation_view)
-        navController = findNavController(R.id.nav_host_fragment)
-        navView.setupWithNavController(navController!!) //the second most important part
+    override fun updateLocale(locale: Locale) {
+        super.updateLocale(locale)
+    }
 
 
 
+    public fun updateLangToAR() {
 
+        // if (sp != null && lang == "ar")
+        updateLocale(Locales.Arabic)
+    }
+    public fun updateLangToEN() {
+        //  if (sp != null && lang == "en")
+       updateLocale(Locales.English)
 
     }
+    public fun updateLangToUR() {
+        //  if (sp != null && lang == "en")
+    //    updateLocale(Locales.Urdu)
+
+    }
+
 
 
     @Override
