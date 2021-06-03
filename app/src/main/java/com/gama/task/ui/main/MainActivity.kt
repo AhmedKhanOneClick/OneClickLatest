c5e4b1ce49d6a0914014795e9525aedc5c9a0a50
Ali Abou-bakr <devabakr@gmail.com>
2021-06-03 10:42:57 +0200

    3 jun Ali backup

--- app/src/main/java/com/gama/task/ui/main/MainActivity.kt
+++ app/src/main/java/com/gama/task/ui/main/MainActivity.kt
@@ -1,34 +1,75 @@
 package com.gama.task.ui.main
 
+import android.app.TaskStackBuilder
+import android.content.Context
+import android.content.Intent
+import android.content.res.Configuration
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import android.view.Menu
+import android.view.MotionEvent
 import androidx.activity.viewModels
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
+import androidx.databinding.DataBindingUtil.setContentView
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.gama.task.R
-import com.gama.task.databinding.ActivityMainBinding
 import com.gama.task.ui.base.BaseActivity
+import com.gama.task.ui.fragments.cart.GlobalClass
+import com.google.android.material.navigation.NavigationView
+import com.zeugmasolutions.localehelper.*
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 @AndroidEntryPoint
-class MainActivity : BaseActivity<ActivityMainBinding>() {
+open class MainActivity : LocaleAwareCompatActivity() {
+    private val viewModel by viewModels<MainViewModel>()
+    lateinit var LocaleAwareCompatActivity: LocaleAwareCompatActivity
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var navController: NavController
     private lateinit var drawer: DrawerLayout
-    private val viewModel by viewModels<MainViewModel>()
-    override fun getLayoutRes() = R.layout.activity_main
-    override fun init() {
-        val toolbar = binding.toolbar
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        drawer = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.main_navigation_view)
+        navController = findNavController(R.id.nav_host_fragment)
+        navView.setupWithNavController(navController!!) //the second most important part
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
-        drawer = binding.drawerLayout
         val toggle: ActionBarDrawerToggle = ActionBarDrawerToggle(
             this, drawer, toolbar,
             R.string.navigation_drawer_open, R.string.navigation_drawer_close
         )
         drawer.addDrawerListener(toggle)
+
         toggle.syncState()
+
+
+
     }
+
+
+
+
+//val opencart=binding.openCartBtn.setOnClickListener {
+
+
+
 
     @Override
     override fun onBackPressed() {
@@ -37,6 +78,68 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
         } else {
             super.onBackPressed()
         }
+
+//observ
+
     }
+
+
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        val listen : MutableLiveData<Int> =  MutableLiveData<Int>()
+        var counter= 0
+        counter=GlobalClass.globalCartList.size
+        listen.setValue(counter) //Initilize with a value
+        listen.observe(this, Observer {
+
+            Log.e("observ",listen.value.toString())
+            cart_counter.text=listen.value.toString()
+
+        })
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    public fun observeCartCounter(){
+
+        cart_counter.text=GlobalClass.globalCartList.size.toString()
+
+
+    }
+
+
+
+
+
+
+    override fun updateLocale(locale: Locale) {
+        super.updateLocale(locale)
+    }
+
+
+
+    public fun updateLangToAR() {
+
+        // if (sp != null && lang == "ar")
+        updateLocale(Locales.Arabic)
+    }
+    public fun updateLangToEN() {
+        //  if (sp != null && lang == "en")
+        updateLocale(Locales.English)
+
+    }
+
+
+    public fun updateLangToUR() {
+        //  if (sp != null && lang == "en")
+        updateLocale(Locales.Urdu)
+
+        // updateLocale(Locales.Arabic)
+
+    }
+
+
+
 
 }
